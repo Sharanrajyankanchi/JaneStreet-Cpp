ebd3d97d95f765c4d4cea90255c378ef788fcc5a
Sharanraj Yankanchi <yankanchisharanraj658@gmail.com>
2025-08-21 12:00:34 +0530

    Update Parenthesis Checker.cpp

--- DSA/gfg-solutions/Parenthesis Checker.cpp
+++ DSA/gfg-solutions/Parenthesis Checker.cpp
@@ -1,11 +1,10 @@
-/*Parenthesis Checker
+/*
+                                              /  Parenthesis Checker  /
 Difficulty: EasyAccuracy: 28.56%Submissions: 688K+Points: 2
 Given a string s, composed of different combinations of '(' , ')', '{', '}', '[', ']', verify the validity of the arrangement.
 An input string is valid if:
-
          1. Open brackets must be closed by the same type of brackets.
          2. Open brackets must be closed in the correct order.
-
 Examples :
 Input: s = "[{()}]"
 Output: true
@@ -23,6 +22,7 @@ Constraints:
 1 ≤ s.size() ≤ 106
 s[i] ∈ {'{', '}', '(', ')', '[', ']'}
 */
+
 class Solution {
 public:
     bool isBalanced(string k) {
