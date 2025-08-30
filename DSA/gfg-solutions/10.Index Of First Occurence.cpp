8e93c5579b9e198c4cc121c2dcef5a952a86961e
Sharanraj Yankanchi <yankanchisharanraj658@gmail.com>
2025-08-30 09:26:05 +0530

    Create 10.Index Of First Occurence.cpp

---
+++ DSA/gfg-solutions/10.Index Of First Occurence.cpp
@@ -0,0 +1,35 @@
+/*                                     10.Index Of First Occurence
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+Constraints:
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+  */
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+ int strStr(string haystack, string needle) {
+        size_t pos = haystack.find(needle); // find once
+        return (pos == string::npos) ? -1 : pos;
+    }
+};
+int main(){ 
+  Solution s;
+    string haystack = "hello";
+    string needle = "ll";
+    int index = s.strStr(haystack, needle);
+    cout <<  index << endl;  // Output: 2
+    return 0;
+}
