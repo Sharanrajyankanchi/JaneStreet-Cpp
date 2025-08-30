8849619515cd9bea07e82cb6ed371c41f2929779
Sharanraj Yankanchi <yankanchisharanraj658@gmail.com>
2025-08-30 21:25:32 +0530

    Create 16.Climbing stairs.cpp

---
+++ DSA/gfg-solutions/16.Climbing stairs.cpp
@@ -0,0 +1,46 @@
+/*                                  16.Climbing stairs
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+  
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+  
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+Constraints:
+1 <= n <= 45
+  */
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a=1,b=1,sum=1;
+while(n>1){sum=b;
+    sum+=a;
+           a=b;
+           b=sum;
+           n--;
+}
+return b;;
+    }
+};
+int main(){
+    Solution sol;
+    int n;
+    cout << "Enter number of stairs: ";
+    cin >> n;
+    cout << "Number of ways to climb " << n << " stairs = " << sol.climbStairs(n) << endl;
+    return 0;
+}
+  
