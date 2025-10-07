e9a3f4692b6752199849436a43d2f72a6204dbcc
Sharanraj Yankanchi <yankanchisharanraj658@gmail.com>
2025-10-07 19:49:00 +0530

    Create 30.Reverse Vowels Of String.cpp

---
+++ DSA/gfg-solutions/30.Reverse Vowels Of String.cpp
@@ -0,0 +1,35 @@
+                                                  30.Reverse Vowels Of String.
+/*
+Given a string s, reverse only all the vowels in the string and return it.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+Example 1:
+Input: s = "IceCreAm"
+Output: "AceCreIm"
+Explanation:
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+Example 2:
+Input: s = "leetcode"
+Output: "leotcede"
+
+Constraints:
+1 <= s.length <= 3 * 105
+s consist of printable ASCII characters.
+*/
+class Solution {
+public:
+    string reverseVowels(string s) {
+        auto isvowel=[](char c){
+            c=tolower(c);
+            return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+        };
+        int j=s.size()-1;int i=0;
+      while(i<j){
+        while(i<j&&!isvowel(s[i])) i++;
+        while(i<j&&!isvowel(s[j])) j--;
+        if(i<j) swap(s[i],s[j]);i++;j--;
+      }
+      return s;
+    }
+};
