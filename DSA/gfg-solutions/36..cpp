99f3e935da6adfcd452e5814a2c4f99dc237f87b
Sharanraj Yankanchi <yankanchisharanraj658@gmail.com>
2025-10-09 10:24:14 +0530

    Create 36..cpp

---
+++ DSA/gfg-solutions/36..cpp
@@ -0,0 +1,32 @@
+
+/*
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+Example 1:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+Example 2:
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+
+Constraints:
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+  */
+ class Solution {
+public:
+    int longestPalindrome(string s) {
+        int a[256]={0};
+        for(char x:s) a[x]++;
+        int i=0;
+        for(int x:a) {
+            if(x%2!=0)i++;
+        }
+        if(i==0) return s.size();
+        return s.size()-i+1;
+    }
+};
